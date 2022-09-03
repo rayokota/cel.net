96d0113af57f88be95f900aca3c9a2549893b9f7
Robert Yokota <rayokota@gmail.com>
2022-09-03 00:09:35 -0700

    checkpoint

---
+++ core/src/test/Cel/Common/SourceTest.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+/*
+ * Copyright (C) 2022 Robert Yokota
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace Cel.Common
+{
+    internal class SourceTest
+    {
+        internal const string unexpectedSnippet = "got snippet '%s', want '%s'";
+        internal const string snippetFound = "snippet found at line %d, wanted none";
+
+        /// <summary>
+        /// the error description method. </summary>
+        [Test]
+        public virtual void Description()
+        {
+            string contents = "example content\nsecond line";
+
+            Source source = Source.NewStringSource(contents, "description-test");
+
+            Assert.That(source.Content(), Is.EqualTo(contents));
+            Assert.That(source.Description(), Is.EqualTo("description-test"));
+
+            // Assert that the snippets on lines 1 & 2 are what was expected.
+            Assert.That(source.Snippet(2), Is.EqualTo("second line"));
+            Assert.That(source.Snippet(1), Is.EqualTo("example content"));
+        }
+
+        /// <summary>
+        /// make sure that the offsets accurately reflect the location of a character in source. </summary>
+        [Test]
+        public virtual void EmptyContents()
+        {
+            Source source = Source.NewStringSource("", "empty-test");
+
+            Assert.That(source.Snippet(1), Is.EqualTo(""));
+
+            string str2 = source.Snippet(2);
+            Assert.That(str2, Is.Null);
+        }
+
+        /// <summary>
+        /// snippets from a single line source. </summary>
+        [Test]
+        public virtual void SnippetSingleline()
+        {
+            Source source = Source.NewStringSource("hello, world", "one-line-test");
+
+            Assert.That(source.Snippet(1), Is.EqualTo("hello, world"));
+
+            string str2 = source.Snippet(2);
+            Assert.That(str2, Is.Null);
+        }
+
+        /// <summary>
+        /// snippets of text from a multiline source. </summary>
+        [Test]
+        public virtual void SnippetMultiline()
+        {
+            IList<string> testLines = new List<string> { "", "", "hello", "world", "", "my", "bub", "", "" };
+
+            Source source = Source.NewStringSource(string.Join("\n", testLines), "mulit-line-test");
+
+            Assert.That(source.Snippet(testLines.Count + 1), Is.Null);
+            Assert.That(source.Snippet(0), Is.Null);
+
+            for (int i = 1; i <= testLines.Count; i++)
+            {
+                string testLine = testLines[i - 1];
+
+                string str = source.Snippet(i);
+                Assert.That(str, Is.EqualTo(testLine));
+            }
+        }
+
+        /// <summary>
+        /// make sure that the offsets accurately reflect the location of a character in source. </summary>
+        [Test]
+        public virtual void LocationOffset()
+        {
+            string contents = "c.d &&\n\t b.c.arg(10) &&\n\t test(10)";
+            Source source = Source.NewStringSource(contents, "offset-test");
+            Assert.That(source.LineOffsets(), Is.EquivalentTo(new List<int>{ 7, 24, 35}));
+
+            // Ensure that selecting a set of characters across multiple lines works as
+            // expected.
+            int charStart = source.LocationOffset(Location.NewLocation(1, 2));
+            int charEnd = source.LocationOffset(Location.NewLocation(3, 2));
+            Assert.That(contents.Substring(charStart, charEnd - charStart), Is.EqualTo("d &&\n\t b.c.arg(10) &&\n\t "));
+            Assert.That(source.LocationOffset(Location.NewLocation(4, 0)), Is.EqualTo(-1));
+        }
+
+        /// <summary>
+        /// Ensure there is no panic when passing nil, NewInfoSource should use proto v2 style accessors.
+        /// </summary>
+        [Test]
+        public virtual void NoPanicOnNil()
+        {
+            // Not implemented - there's no 'nil' in Java
+            //  _ = NewInfoSource(nil)
+        }
+    }
+}
---
+++ core/src/test/Cel/Parser/UnescapeTest.cs
@@ -0,0 +1,189 @@
+﻿/*
+ * Copyright (C) 2022 Robert Yokota
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Text;
+using Cel.Parser;
+using NUnit.Framework;
+
+namespace Cel.Parser
+{
+    internal class UnescapeTest
+    {
+[Test]
+        public virtual void UnescapeSingleQuote()
+        {
+            string text = Utf8(Unescape.DoUnescape("'hello'", false));
+            Assert.That(text, Is.EqualTo("hello"));
+        }
+
+[Test]
+        public virtual void UnescapeDoubleQuote()
+        {
+            string text = Utf8(Unescape.DoUnescape("\"\"", false));
+            Assert.That(text, Is.EqualTo(""));
+        }
+
+[Test]
+        public virtual void UnescapeEscapedQuote()
+        {
+            // The argument to unescape is dquote-backslash-dquote-dquote where both
+            // the backslash and inner double-quote are escaped.
+            string text = Utf8(Unescape.DoUnescape("\"\\\\\\\"\"", false));
+            Assert.That(text, Is.EqualTo("\\\""));
+        }
+
+[Test]
+        public virtual void UnescapeEscapedEscape()
+        {
+            string text = Utf8(Unescape.DoUnescape("\"\\\\\"", false));
+            Assert.That(text, Is.EqualTo("\\"));
+        }
+
+[Test]
+        public virtual void UnescapeTripleSingleQuote()
+        {
+            string text = Utf8(Unescape.DoUnescape("'''x''x'''", false));
+            Assert.That(text, Is.EqualTo("x''x"));
+        }
+
+[Test]
+        public virtual void UnescapeTripleDoubleQuote()
+        {
+            string text = Utf8(Unescape.DoUnescape("\"\"\"x\"\"x\"\"\"", false));
+            Assert.That(text, Is.EqualTo("x\"\"x"));
+        }
+
+[Test]
+        public virtual void UnescapeMultiOctalSequence()
+        {
+            // Octal 303 -> Code point 195 (Ã)
+            // Octal 277 -> Code point 191 (¿)
+            string text = Utf8(Unescape.DoUnescape("\"\x00C3\x00BF\"", false));
+            Assert.That(text, Is.EqualTo("Ã¿"));
+        }
+
+[Test]
+        public virtual void UnescapeOctalSequence()
+        {
+            // Octal 377 -> Code point 255 (ÿ)
+            string text = Utf8(Unescape.DoUnescape("\"\x00FF\"", false));
+            Assert.That(text, Is.EqualTo("ÿ"));
+        }
+
+[Test]
+        public virtual void UnescapeUnicodeSequence()
+        {
+            string text = Utf8(Unescape.DoUnescape("\"\u263A\u263A\"", false));
+            Assert.That(text, Is.EqualTo("☺☺"));
+        }
+
+[Test]
+        public virtual void UnescapeLegalEscapes()
+        {
+            string text = Utf8(Unescape.DoUnescape("\"\\a\\b\\f\\n\\r\\t\\v\\'\\\"\\\\\\? Legal escapes\"", false));
+            Assert.That(text, Is.EqualTo("\x0007\b\f\n\r\t\x000B'\"\\? Legal escapes"));
+        }
+
+[Test]
+        public virtual void UnescapeIllegalEscapes()
+        {
+            // The first escape sequences are legal, but the '\>' is not.
+            Assert.That(() => Unescape.DoUnescape("\"\\a\\b\\f\\n\\r\\t\\v\\'\\\"\\\\\\? Illegal escape \\>\"", false),
+                Throws.Exception.TypeOf(typeof(System.ArgumentException)));
+        }
+
+[Test]
+        public virtual void UnescapeBytesAscii()
+        {
+            string bs = Utf8(Unescape.DoUnescape("\"abc\"", true));
+            Assert.That(bs, Is.EqualTo("abc"));
+        }
+
+[Test]
+        public virtual void UnescapeBytesUnicode()
+        {
+            byte[] bs = Bytes(Unescape.DoUnescape("\"ÿ\"", true));
+            Assert.That(bs, Is.EqualTo(new byte[] { unchecked((byte)0xc3), unchecked((byte)0xbf) }));
+        }
+
+[Test]
+        public virtual void UnescapeBytesOctal()
+        {
+            byte[] bs = Bytes(Unescape.DoUnescape("\"\\303\\277\"", true));
+            Assert.That(bs, Is.EqualTo(new byte[] { unchecked((byte)0xc3), unchecked((byte)0xbf) }));
+        }
+
+[Test]
+        public virtual void UnescapeBytesOctalMax()
+        {
+            byte[] bs = Bytes(Unescape.DoUnescape("\"\\377\"", true));
+            Assert.That(bs, Is.EqualTo(new byte[] { unchecked((byte)0xff) }));
+        }
+
+[Test]
+        public virtual void UnescapeBytesQuoting()
+        {
+            string bs = Utf8(Unescape.DoUnescape("'''\"Kim\\t\"'''", true));
+            Assert.That(bs,
+                Is.EqualTo(new string(new char[]
+                    { (char)0x22, (char)0x4b, (char)0x69, (char)0x6d, (char)0x09, (char)0x22 })));
+        }
+
+[Test]
+        public virtual void UnescapeBytesHex()
+        {
+            byte[] bs = Bytes(Unescape.DoUnescape("\"\\xc3\\xbf\"", true));
+            Assert.That(bs, Is.EqualTo(new byte[] { unchecked((byte)0xc3), unchecked((byte)0xbf) }));
+        }
+
+[Test]
+        public virtual void UnescapeBytesHexMax()
+        {
+            byte[] bs = Bytes(Unescape.DoUnescape("\"\\xff\"", true));
+            Assert.That(bs, Is.EqualTo(new byte[] { unchecked((byte)0xff) }));
+        }
+
+[Test]
+        public virtual void UnescapeBytesUnicodeEscape()
+        {
+            Assert.That(() => Unescape.DoUnescape("\"\\u00ff\"", true),
+                Throws.Exception.TypeOf(typeof(System.ArgumentException)));
+        }
+
+        // TODO
+        /*
+[Test]
+        public virtual void InvalidUtf8()
+        {
+            Assert.That(() => Unescape.ToUtf8(new MemoryStream(new byte[] { 0, unchecked((byte)255) })),
+                Throws.Exception.TypeOf(typeof(Exception)));
+        }
+        */
+
+        private static byte[] Bytes(MemoryStream unescape)
+        {
+            byte[] bytes = new byte[unescape.Length];
+            unescape.Read(bytes);
+            return bytes;
+        }
+
+        private static string Utf8(MemoryStream unescape)
+        {
+            byte[] bytes = Bytes(unescape);
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
