0da312ff9fa1b35d361d5078b5669d07e1609ad5
Robert Yokota <rayokota@gmail.com>
2022-09-05 17:38:24 -0700

    checkpoing conform

--- src/conformance/Cel/Server/ConformanceServer.cs
+++ src/conformance/Cel/Server/ConformanceServer.cs
@@ -32,14 +32,12 @@ namespace Cel.Server
 			Grpc.Core.Server server = new Grpc.Core.Server
 			{
 				Services = { ConformanceService.BindService(new ConformanceServiceImpl()) },
-				Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+				Ports = { new ServerPort("127.0.0.1", Port, ServerCredentials.Insecure) }
 			};
 			server.Start();
 
-			Console.WriteLine("Greeter server listening on port " + Port);
-			Console.WriteLine("Press any key to stop the server...");
-			Console.ReadKey();
-
+			Console.WriteLine("Listening on {0}:{1}", "127.0.0.1", Port);
+			Thread.Sleep(int.MaxValue);
 			server.ShutdownAsync().Wait();
 		}
 	}
