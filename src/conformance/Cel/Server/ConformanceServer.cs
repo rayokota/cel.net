f3826a0d12ad49bea330b5a7327acd6453b46279
Robert Yokota <rayokota@gmail.com>
2022-09-06 17:39:09 -0700

    ASP impl

--- src/conformance/Cel/Server/ConformanceServer.cs
+++ src/conformance/Cel/Server/ConformanceServer.cs
@@ -27,7 +27,7 @@ namespace Cel.Server
 	{
 		const int Port = 30051;
 
-		public static void Main(string[] args)
+		public static void Main2(string[] args)
 		{
 			Grpc.Core.Server server = new Grpc.Core.Server
 			{
--- src/conformance/Cel/Server/Program.cs
+++ src/conformance/Cel/Server/Program.cs
@@ -12,7 +12,7 @@ namespace Cel.Server
 {
     public class Program
     {
-        public static void Main2(string[] args)
+        public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
