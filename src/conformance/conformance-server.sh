25ce7ad59fc7066052a5d4e0249c0fb566ceb119
Robert Yokota <rayokota@gmail.com>
2022-09-11 22:16:28 -0700

    internal to private

--- src/conformance/conformance-server.sh
+++ src/conformance/conformance-server.sh
@@ -27,7 +27,8 @@ function kill_server() {
 
 trap kill_server SIGINT SIGTERM
 
-dotnet run --project "${wd}" 2>&1 | tee /tmp/conformance-server.log | grep Listening &
+#dotnet run --project "${wd}" 2>&1 | tee /tmp/conformance-server.log | grep Listening &
+dotnet run --project "${wd}" | grep Listening
 #echo "Listening on localhost:5000"
 
 #(dotnet ${wd}/bin/Debug/net6.0/Cel.conformance.dll & echo $! > "${pid_file}") 2>&1 | tee /tmp/conformance-server.log | grep Listening
